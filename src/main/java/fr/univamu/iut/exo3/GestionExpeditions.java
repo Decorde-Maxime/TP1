36aa363a8970753a0b4a0e0d9155398697f92ed8
d16013755 <maxime.decorde@etu.univ-amu.fr>
2017-10-12 12:14:00 +0200

    Exos incomplets

--- src/main/java/fr/univamu/iut/exo3/GestionExpeditions.java
+++ src/main/java/fr/univamu/iut/exo3/GestionExpeditions.java
@@ -4,6 +4,7 @@ import fr.univamu.iut.Conteneur;
 import fr.univamu.iut.exo1.*;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 
 public class GestionExpeditions
@@ -12,16 +13,7 @@ public class GestionExpeditions
     {
         try
         {
-            List<Conteneur> conteneurs = fr.univamu.iut.exo1.Lecteur.charger ("Conteneurs.ser");
-            for (Conteneur con : conteneurs)
-                System.out.println(con.toString());
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        assertTrue (Arrays.equals(conteneurs.toArray(), Lecteur.charger("Conteneurs.ser").toArray()));
-    }
+            Collection<Conteneur> pile = Lecteur.charger()
 }
 
 
